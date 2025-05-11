81760e3ac0838a55e6ecaace273dfc517c1fc393
basamba1990 <basamba1990@yahoo.fr>
2025-05-11 01:43:40 +0000

    Update vite-env.d.ts

--- front-end/src/vite-env.d.ts
+++ front-end/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+  // Ajoute ici d'autres variables d'environnement VITE_ si besoin
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
