1d753c299583f0412b5be7625aa3d7280896d9d9
basamba1990 <basamba1990@yahoo.fr>
2025-05-20 00:53:41 +0000

    Add files via upload

--- backend/app/schemas/token_schema.py
+++ backend/app/schemas/token_schema.py
@@ -5,8 +5,13 @@ from typing import Optional
 
 class Token(BaseModel):
     access_token: str
+    refresh_token: str
     token_type: str
+    user_id: Optional[int] = None
+    is_superuser: Optional[bool] = None
 
 class TokenData(BaseModel):
     email: Optional[str] = None
 
+class RefreshToken(BaseModel):
+    refresh_token: str
