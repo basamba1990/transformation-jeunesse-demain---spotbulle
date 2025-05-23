ebb584f09a37b20d201edb9e199764cb42d336eb
basamba1990 <basamba1990@yahoo.fr>
2025-05-23 01:42:45 +0000

    Update __init__.py

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,15 +1,14 @@
-
 """
 Fichier d'initialisation pour le module routes.
 Importe tous les routeurs pour les rendre disponibles lors de l'initialisation de l'application.
 """
 
-from .auth_routes import router as auth_router
-from .user_routes import router as user_router
-from .pod_routes import router as pod_router
-from .profile_routes import router as profile_router
-from .ia_routes import router as ia_router
-from .video_routes import router as video_router
+from app.routes.auth_routes import router as auth_router
+from app.routes.user_routes import router as user_router
+from app.routes.pod_routes import router as pod_router
+from app.routes.profile_routes import router as profile_router
+from app.routes.ia_routes import router as ia_router
+from app.routes.video_routes import router as video_router
 
 # Exporte tous les routeurs pour qu'ils soient accessibles via routes.*
 __all__ = [
