cd60df360745182d148c123d78af61916de907c0
basamba1990 <basamba1990@yahoo.fr>
2025-05-23 03:13:13 +0000

    Update __init__.py

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,5 @@
-from ..user_model import User
-from ..profile_model import Profile
-from ..pod_model import Pod
+from .user_model import User
+from .profile_model import Profile
+from .pod_model import Pod
 
 __all__ = ["User", "Profile", "Pod"]
