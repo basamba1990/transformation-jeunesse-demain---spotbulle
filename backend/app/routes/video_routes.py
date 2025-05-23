2a3b4bb384c06483389e008b901cf13544dda000
basamba1990 <basamba1990@yahoo.fr>
2025-05-23 09:54:31 +0000

    Update video_routes.py

--- backend/app/routes/video_routes.py
+++ backend/app/routes/video_routes.py
@@ -5,10 +5,10 @@ import logging
 import tempfile
 import os
 
-from ..schemas import pod_schema, user_schema
-from ..services import pod_service, storage_service, transcription_service, video_service
-from ..utils import security
-from ..database import get_db
+from .schemas import pod_schema, user_schema
+from .services import pod_service, storage_service, transcription_service, video_service
+from .utils import security
+from .database import get_db
 
 from slowapi import Limiter
 from slowapi.util import get_remote_address
