cfe3247f4421c8c78c436121713da77dedfe5043
basamba1990 <basamba1990@yahoo.fr>
2025-05-24 07:20:26 +0000

    Update api.ts

--- front-end/src/services/api.ts
+++ front-end/src/services/api.ts
@@ -3,7 +3,7 @@ import type { DISCScores, DISCResults } from "../schemas/disc_schema";
 
 // Configuration de la base URL
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 
-  "https://spotbulle-backend-sack.onrender.com/api/v1";
+  "https://spotbulle-backend-tydv.onrender.com/api/v1";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
