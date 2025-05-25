9d6112832082987f07d5603c1a79953126900c5a
basamba1990 <basamba1990@yahoo.fr>
2025-05-25 02:20:17 +0000

    Delete backend/app/routes/correction_auth_routes.py

--- backend/app/routes/correction_auth_routes.py
+++
@@ -1,7 +0,0 @@
-# Correction pour routes/auth_routes.py
-
-# Remplacer cette ligne dans la fonction register_user:
-# return user_schema.User.from_orm(created_user)
-
-# Par celle-ci:
-return user_schema.User.model_validate(created_user)
