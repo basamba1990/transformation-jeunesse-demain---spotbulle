e28b3f2b6974fc84234c64ddc1b25426f5b60b8e
basamba1990 <basamba1990@yahoo.fr>
2025-06-01 13:04:10 +0000

    Add files via upload

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,6 +5,12 @@ import { AuthProvider } from './contexts/AuthContext';
 import MainLayout from './layout/MainLayout';
 import HomePage from './pages/HomePage';
 import ResourcesPage from './pages/ResourcesPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import PodsPage from './pages/PodsPage';
+import PodsCreatePage from './pages/PodsCreatePage';
+import MatchesPage from './pages/MatchesPage';
 import './index.css';
 
 const App = () => {
@@ -21,6 +27,41 @@ const App = () => {
             <Route path="/resources" element={
               <MainLayout>
                 <ResourcesPage />
+              </MainLayout>
+            } />
+            <Route path="/login" element={
+              <MainLayout>
+                <LoginPage />
+              </MainLayout>
+            } />
+            <Route path="/register" element={
+              <MainLayout>
+                <RegisterPage />
+              </MainLayout>
+            } />
+            <Route path="/profile" element={
+              <MainLayout>
+                <ProfilePage />
+              </MainLayout>
+            } />
+            <Route path="/profile/me" element={
+              <MainLayout>
+                <ProfilePage />
+              </MainLayout>
+            } />
+            <Route path="/pods" element={
+              <MainLayout>
+                <PodsPage />
+              </MainLayout>
+            } />
+            <Route path="/pods/create" element={
+              <MainLayout>
+                <PodsCreatePage />
+              </MainLayout>
+            } />
+            <Route path="/matches" element={
+              <MainLayout>
+                <MatchesPage />
               </MainLayout>
             } />
             <Route path="*" element={
